32108b5ac9a629184687e31d98d8a1dde71b35f5
Rafael <rafinha_fern@hotmail.com>
2022-05-01 03:56:05 -0300

    requisição post novo login, bibliotecas e validações com tratamento de erro

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
+// imports
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
+import Joi from "joi";
+import dayjs from "dayjs";
+import { MongoClient } from "mongodb";
+// levantar servidor e conexão mongoDB
+const mongoClient = new MongoClient("mongodb://localhost:27017");
 const app = express();
-app.use(cors())
-app.get("/",(req,res)=>{
-    console.log("ta indo")
-    res.send("Hello world!")
-})
+//
+mongoClient.connect();
+app.use(cors());
+app.use(express.json());
+//
+let db = mongoClient.db("batepapo_uol");
+app.post("/", async (req, res) => {
+  const name = req.body;
+  const nameSchema = Joi.object({
+    name: Joi.string().required()
+  });
+  const getNameValidation = nameSchema.validate(name, { abortEarly: false });
+  if (getNameValidation.error) {
+    res.status(422).send(getNameValidation.error.details);
+    return;
+  }
+  try {
+    const newUser = await db.collection("login").findOne({name:name.name});
+    if(newUser) {
+        res.status(409).send("Deu ruim, já tem esse nome...")
+        return
+    }
+    await db
+      .collection("login")
+      .insertOne({ ...name, lastStatus: Date.now() });
+    await db
+      .collection("login")
+      .insertOne({
+        from: "xxx",
+        to: "Todos",
+        text: "entra na sala...",
+        time: dayjs().format("HH:mm:ss"),
+      });
+      console.log("deu bom")
+  } catch(e) {
+      console.log(e)
+  }
+  res.send("foi enviado o bagulho");
+});
 app.listen(5000);
