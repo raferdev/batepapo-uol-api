e4a3e601f3ff9f678474335808217b9ff0e46854
Rafael <rafinha_fern@hotmail.com>
2022-05-03 02:22:22 -0300

    operação get e post messages feitas faltando filtragem type e mensagens filtradas no get

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post("/participants", async (req, res) => {
       return;
     }
     await db.collection("login").insertOne({ ...name, lastStatus: Date.now() });
-    await db.collection("login").insertOne({
+    await db.collection("messages").insertOne({
       from: name.name,
       to: "Todos",
       text: "entra na sala...",
@@ -39,15 +39,58 @@ app.post("/participants", async (req, res) => {
     console.log("deu bom");
   } catch (e) {
     console.log(e);
+    res.status(422);
   }
   res.send("foi enviado o bagulho");
 });
 app.get("/participants", async (req, res) => {
   try {
     const participants = await db.collection('login').find().toArray()
-    console.log(participants);
+    res.status(201).send(participants);
   } catch (e) {
+    res.status(422).send('deu ruim ao procurar os participantes')
     console.log('deu xabu',e)
   }
+});
+app.post('/messages',async (req,res) => {
+  const massage = req.body;
+  const user = req.headers.user;
+  const messageScheme =  Joi.object({
+    to: Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().required()
+  });
+  const validate = messageScheme.validate(massage,{ abortEarly: false });
+  if(!validate) {
+    return res.status(422).send(validate.error.details.massage)
+  }
+  try {
+  const validaUser =  await db.collection('login').findOne();
+  if(!validaUser) {
+    console.log('usuario nao encontrando');
+    return res.status(422).send('usuario não encontrado');
+  }
+  } catch(e) {
+    console.log('deu ruim ao procurar o nome');
+    res.status(422).send('deu ruim ao procurar o nome');
+  }
+  try {
+    await db.collection('messages').insertOne({from:user,...massage,time:dayjs().format("HH:mm:ss")})
+    res.sendStatus(201);
+  } catch(e) {
+    console.log(e);
+    res.sendStatus(422);
+  }
+});
+app.get('/messages',async (req,res)=>{
+  const {user} = req.header;
+  const limit = parseInt(req.query.limit);
+  try {
+    const messages = await db.collection('messages').find().toArray();
+    res.status(201).send(messages);
+  } catch(e) {
+    console.log('deu ruim pra pegar as msgs');
+    res.sendStatus(422)
+  };
 });
 app.listen(5000);
